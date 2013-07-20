b10fd2ecc45f4a71c4c6ce0a379da25e1db41096
Holger Ballweg <hb@uiae.de>
2013-07-20 17:36:48 +0200

    Instruments: Add tests. Define instrument fn in current ns.

--- test/frankentone/instruments_test.clj
+++ test/frankentone/instruments_test.clj
@@ -10,3 +10,23 @@
   (is (not (contains? @instruments :inst-test)))
   (is (not= (definst inst-test (fn [x y z a] 0.0)) nil))
   (is (contains? @instruments :inst-test)))
+
+(deftest play-note-test
+  (is (do (definst inst-test (fn [x y z a] 1.0))
+          (play-note 1.0 :inst-test 440.0 1.0 1.0)
+          (= (inst-test 0.05) 0.0)))
+  (is (= (inst-test 1.0) 1.0))
+  (is (= (inst-test 1.5) 1.0))
+  (is (= (inst-test 2.0) 0.0))
+  (is (= (inst-test 20.0) 0.0)))
+
+(deftest play-and-clear-test
+  (is (do (definst inst-test (fn [x y z a] 1.0))
+          (dotimes [i 20]
+            (play-note (rand 1.0) :inst-test 440.0 1.0 1.0))
+          (inst-test 1.0)
+          (and (= (count @(.notes (:inst-test @instruments))) 20)
+               (= (.peek (.note-starts (:inst-test @instruments))) nil))))
+  (is (do (clear (:inst-test @instruments))
+          (and (= @(.notes (:inst-test @instruments)) {})
+               (= (.peek (.note-starts (:inst-test @instruments))) nil)))))
