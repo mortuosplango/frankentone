8480665b7ae1f28ab4fde91cf355b35b17a5cef0
Holger Ballweg <hb@uiae.de>
2014-01-31 21:39:55 +0000

    Live: redefine bset! and bget to fix problems in live ns

--- src/frankentone/live.clj
+++ src/frankentone/live.clj
@@ -1,13 +1,35 @@
 (ns frankentone.live
-  (:require [frankentone.api]))
+  (:require [frankentone api]
+            [clojure repl]))
 
 (frankentone.api/immigrate-frankentone-api)
 
-;; Redefine buffer (defined in frankentone.genetic.utils).
+;; Redefine buffer, bset! and bget (defined in frankentone.genetic.dspgp).
 ;;
 ;; Seems to be necessary as Clojure otherwise says it's not dynamic
 ;; though the metadata is correctly copied.
 
 (def ^:dynamic buffer
   (double-array *sample-rate* 0.0))
+
+
+(defn bset!
+  "Set the buffer at second x to value y. Returns the former value at
+  x.
+
+  As the buffer is just one second long, other values for x will be
+  wrapped."
+  ^double [x y]
+  (let [pos (* (pmod x 1.0) (dec *sample-rate*))
+        previous-value (hiphip.double/aget buffer pos)]
+    (hiphip.double/aset buffer pos ^double y)
+    previous-value))
+
+
+(defn bget
+  "Get the value at second x in the buffer.
+
+  As the buffer is just one second long, other values for x will be
+  wrapped."
+  ^double [x] (hiphip.double/aget buffer (* (pmod x 1.0) (dec *sample-rate*))))
 
