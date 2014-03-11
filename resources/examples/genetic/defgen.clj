2d7683b7f4ddcaf56a614d0f0ace9d75499dda64
Holger Ballweg <hb@uiae.de>
2014-03-11 11:31:19 +0000

    examples/genetic/defgen: comment and expand example

--- resources/examples/genetic/defgen.clj
+++ resources/examples/genetic/defgen.clj
@@ -1,45 +1,59 @@
 (ns frankentone.examples.genetic.defgen
   (:use [frankentone live]))
 
-(instruments->dsp!)
-
-
-(defpat tst88 [#{40.751 45.739}] 
-	:duration 4.0 :instrument :vl)
+;; define some genetic processes and seed them
+;; replace the seed with nil if you want to start the process without
+(defgen ghh 'hh (load-sample (clojure.java.io/resource "hihat-open.wav")) 
+	(mul-cos (mean TAU prev) (mean TAU (mean TAU prev))))
 	
+(defgen+ugens gbd 'bd (load-sample (clojure.java.io/resource "kick.wav"))
+ (frankentone.ugens/sine (frankentone.ugens/delay-c (tanh (- TAU)) Math/PI (frankentone.ugens/bpf-c (tanh 8.39542351397461) prev prev) -0.9215224350650486) -3.9201090149492357))
+
+(defgen+ugens gsn 'sn (load-sample (clojure.java.io/resource "snare-chili.wav")) 
+	(frankentone.ugens/impulse-c 22.92067271872942 (min 599.4559957416726 *sample-rate*) -0.7079338992052633))
+
+(defgen+ugens gvl 'vl (load-sample (clojure.java.io/resource "nasty-violin-4.wav")) 
+	(tanh (min (frankentone.ugens/pulsedpw-c 42.824309546975826 (frankentone.ugens/pulsedpw-c (plfsaw Math/PI (pd 1089.385530174594 42.824309546975826)) (plfsaw (sin prev) *sample-rate*) 61.254611950597855) (rrand -1.3028660588130894 1089.385530174594)) Math/PI)))
+
+;; play all instruments and start the dsp loop
+(instruments->dsp!)
+(start-dsp)
+
+;; you can see a table of the individuals and their fitness:
+(gui ghh)
+
+;; define a rhythmic pattern with the above defined instruments
 (defpat tst90 [(shuffle [vl bd bd hh]) sn [bd]])
 
-
-(setVolume (:blub @instruments) 0.5)
-
+;; start and stop it
 (start tst90)
-(start tst88)
-(stop tst88)
 (stop tst90)
 
+
+;; define another pattern with a chord in one of the instruments
+(defpat tst88 [#{62.986 42.098 79.9}] 
+	:duration 4.0 :instrument :vl)
+
+(start tst88)
+(stop tst88)
+
+
+;; set the volume of an instrument
+(setVolume (:vl @instruments) 0.5)
+
+;; evolve the next generation for one or more of the genetic processes
 (next-gen gbd)
 (next-gen gsn)
 (next-gen ghh)
 (next-gen gvl)
-(next-gen gmg)
 
-(stop-auto gbd)
-(stop-auto gvl)
-
+;; start an auto-evolution 
+;; (evolve one generation after the other with a delay of 100ms
+;; in between the calculations)
 (start-auto gbd :delay 100)
 (start-auto ghh :delay 100)
 (start-auto gvl :delay 100)
 
-(defgen ghh 'hh (load-sample (clojure.java.io/resource "hihat-open.wav")) 
-	(mul-sin (tanh (min x 1387.0946914781023)) (if>0 12.025591277372195 (cos (* 11260.143554536115 prev *sample-rate*)) 582.0211638510966)))
-	
-(defgen+ugens gbd 'bd (load-sample (clojure.java.io/resource "kick.wav"))
- (frankentone.ugens/sine (frankentone.ugens/delay-c -4.327505680752827 Math/PI x -0.9215224350650486) -3.9201090149492357))
-
-(defgen+ugens gsn 'sn (load-sample (clojure.java.io/resource "snare-chili.wav")) 
-	(+ (frankentone.ugens/pink-c) (frankentone.ugens/bpf-c -0.32626803834574947 Math/PI (min 4.692821647267513 197.64356934620585))))
-
-(defgen+ugens gvl 'vl (load-sample 	"/Users/hb/Desktop/nasty-violin-4.wav") 
-	(frankentone.ugens/lpf-c (flerp x Math/PI (* (frankentone.ugens/impulse-c *sample-rate* x -0.2650997136200133) 600.0410348445135)) (- 24.5715017794811) Math/PI))
-	
-(next-gen ghh)
+;; stop them again
+(stop-auto gbd)
+(stop-auto gvl)
